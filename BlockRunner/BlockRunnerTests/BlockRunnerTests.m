0a44b4fb99d7dacf6a06a77249489bc33b57d6c6
Brittny Wright <brittnywright@Brittnys-MacBook-Air.local>
2014-05-09 10:06:51 -0600

    test- commit

---
+++ BlockRunner/BlockRunnerTests/BlockRunnerTests.m
@@ -0,0 +1,34 @@
+//
+//  BlockRunnerTests.m
+//  BlockRunnerTests
+//
+//  Created by Brittny Wright on 5/9/14.
+//  Copyright (c) 2014 vientapps. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BlockRunnerTests : XCTestCase
+
+@end
+
+@implementation BlockRunnerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
